bd92450979ead030fb7fbbee1b2a033919f2d1b4
ko <vtg0502@gmail.com>
2019-04-26 10:22:24 +0700

    thi het module

--- cau1.js
+++ cau1.js
@@ -1,11 +1,14 @@
 function findMinScore(arr) {
     let min = arr[0];
-    for (let value of arr) {
-        if (min > value) {
-            min = value;
+    if (arr !== null) {
+        for (let value of arr) {
+            if (min > value) {
+                min = value;
+            }
         }
     }
     return min;
 }
 
+console.log(findMinScore([]))
 console.log(findMinScore([10, 5, 2, 8, -1, 6, 8]));
--- cau2.js
+++ cau2.js
@@ -1,10 +1,14 @@
 function tryRemoveFrmArray(arr, index) {
-    for (let i = 0; i < arr.length; i++) {
-        if (index === i) {
-            arr.splice(index, 1);
+    if (arr !== null) {
+        for (let i = 0; i < arr.length; i++) {
+            if (index === i) {
+                arr.splice(index, 1);
+            }
         }
     }
     return arr;
 }
-console.log(tryRemoveFrmArray([12,6,3,7,21,5,7],2));
+
+console.log(tryRemoveFrmArray([12, 6, 3, 7, 21, 5, 7], 2));
+console.log(tryRemoveFrmArray([], 2));
 
