38df2be0ef736099518fb1adfacbc9cdc8df33fa
ko <vtg0502@gmail.com>
2019-04-26 10:26:38 +0700

    thi het module

--- cau3.js
+++ cau3.js
@@ -1,11 +1,15 @@
 function isPrime(number) {
-    if (number < 2) return false;
-    else {
-        for (let i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i === 0) return false;
+    if(!isNaN(number)){
+        if (number < 2) return false;
+        else {
+            for (let i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i === 0) return false;
+            }
         }
+        return true;
     }
-    return true;
+
 }
 console.log(isPrime(12));
 console.log(isPrime(17));
+console.log(isPrime('b'));
